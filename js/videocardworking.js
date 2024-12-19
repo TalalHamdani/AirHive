4531949402bc81b8e9a6292cd8c0eab8e3b7b69c
AhmadJ005 <241479@students.au.edu.pk>
2024-12-19 16:03:22 +0500

    update3

---
+++ js/videocardworking.js
@@ -0,0 +1,19 @@
+const videos = [
+    {
+        url: "https://www.example.com/video-1",
+        thumbnail: "https://res.cloudinary.com/dpsssw48o/image/upload/fl_preserve_transparency/v1734275880/90280_ou04id.jpg",
+        title: "Video Title 1",
+        channel: "Channel Name 1",
+        views: "1M views",
+        time: "1 hour ago"
+    },
+    {
+        url: "https://www.example.com/video-2",
+        thumbnail: "https://res.cloudinary.com/dpsssw48o/image/upload/fl_preserve_transparency/v1734275880/90280_ou04id.jpg",
+        title: "Video Title 2",
+        channel: "Channel Name 2",
+        views: "500K views",
+        time: "2 hours ago"
+    },
+    // Add more videos as necessary
+];
